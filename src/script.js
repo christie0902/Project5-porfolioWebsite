7066e878bfdcac0ec2329cc3cd891a0a48df058b
Christie Pham <115462474+christie0902@users.noreply.github.com>
2024-02-04 20:01:16 +0100

    add smooth scrolling by js

--- src/script.js
+++ src/script.js
@@ -1,11 +1,25 @@
-const burgerIcon = document.querySelector('.fa-bars')
-const mobileMenu = document.querySelector('.mobile-menu')
-const closeIcon = document.querySelector('.close-icon')
+const burgerIcon = document.querySelector(".fa-bars");
+const mobileMenu = document.querySelector(".mobile-menu");
+const closeIcon = document.querySelector(".close-icon");
 
 burgerIcon.addEventListener("click", (e) => {
-    mobileMenu.classList.add('mobile-menu--active');
-})
+  mobileMenu.classList.add("mobile-menu--active");
+});
 
 closeIcon.addEventListener("click", (e) => {
-    mobileMenu.classList.remove('mobile-menu--active');
-})
+  mobileMenu.classList.remove("mobile-menu--active");
+});
+
+// add smooth scrolling behavior
+
+let navLink = document.querySelectorAll(".nav a");
+navLink.forEach((link) => {
+    link.onclick = function (e) {
+    // Prevent scrolling if the hash value is blank
+    e.preventDefault();
+    let destination = document.querySelector(this.hash);
+    destination.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
+});
